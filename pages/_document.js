1d040ed697b5132cfde0292fe6d36e402b54c576
nitayhillel <nithay1@gmail.com>
2023-06-06 23:20:27 +0300

    add pwa support

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-            <link rel="shortcut icon" href="/logo192.png" />
-            <title>Zablan</title>
-            </Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link rel="shortcut icon" href="/logo192.png" />
+        <title>Zablan</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="green" />
+
+      </Head>
       <body>
         <Main />
         <NextScript />
