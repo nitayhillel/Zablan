3de3057710682bbdb1270144c9d65085944a8a34
nitayhillel <nithay1@gmail.com>
2023-06-07 17:08:25 +0300

    Edit bottom sheet too low

--- components/BottomSheet.js
+++ components/BottomSheet.js
@@ -38,7 +38,7 @@ export default function BottomSheet(props) {
       ))
 
     return (
-        <div className={`transition-all ease-in-out duration-500 h-[50vh] bg-[#f1f3f4] shadow-[0_-5px_10px_0px_rgba(0,0,0,0.14)] w-screen text-8xl text-black fixed bottom-0 rounded-xl ${!sheet && "translate-y-[calc(50vh-30px)]"}`}>
+        <div className={`transition-all ease-in-out duration-500 h-[50vh] bg-[#f1f3f4] shadow-[0_-5px_10px_0px_rgba(0,0,0,0.14)] w-screen text-8xl text-black fixed bottom-0 rounded-xl ${!sheet && "translate-y-[calc(50vh-50px)]"}`}>
             <button className={`transition-transform ease-in-out duration-500 material-symbols-outlined mdcursor mt-3 top-0 left-1/2 right-1/2 text-center absolute text-gray-500 ${!sheet?"-rotate-90":"rotate-90"}`} onClick={handleArrowClick}>arrow_forward_ios</button>
             <div className={`mt-16 overflow-scroll h-full scrollbar-hide`}>
             {soonGezemStreets}
