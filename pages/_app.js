dd0dd853a43962254883d770350da474ec518d06
nitayhillel <nithay1@gmail.com>
2023-06-08 11:14:34 +0300

    Add meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,14 @@ import Head from 'next/head'
 export default function App({ Component, pageProps }) {
   return <>
   <Head>
-  <title>Zablan</title>
+      <title>Zablan</title>
+      <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+      <meta charSet="utf-8" />
+      <meta name="theme-color" content="#4DB685" />
+      <meta
+        name="description"
+        content="לאנשים שאוהבים שימוש חוזר, יצירה והרפתקאות."
+      />
   </Head>
   <Provider store={store}>
         <Wrapper/>
