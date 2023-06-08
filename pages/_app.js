9a47f00cb5a90dce69fc6feca04f280b28357015
nitayhillel <nithay1@gmail.com>
2023-06-08 11:11:03 +0300

    Move title to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,15 @@ import '@/styles/globals.css'
 import { Provider } from 'react-redux'
 import { store } from '@/store'
 import Wrapper from '@/components/Wrapper'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Provider store={store}>
+  return <>
+  <Head>
+  <title>Zablan</title>
+  </Head>
+  <Provider store={store}>
         <Wrapper/>
   </Provider>
+  </>
 }
