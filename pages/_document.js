4170af0ed824e915afde226db6ff6b2dffa0f403
nitayhillel <nithay1@gmail.com>
2023-06-24 10:59:56 +0300

    Fix meta and link tags

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-        <link rel="shortcut icon" href="/logofull.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#4DB685" />
-
+       
       </Head>
       <body>
         <Main />
