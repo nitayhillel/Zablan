13ad749e99367e2bd964793f9874e13ffaf5334c
nitayhillel <nithay1@gmail.com>
2023-07-12 11:22:52 +0300

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import { store } from '@/store'
 import Wrapper from '@/components/Wrapper'
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return <>
@@ -23,6 +24,7 @@ export default function App({ Component, pageProps }) {
     </Head>
     <Provider store={store}>
       <Wrapper />
+      <Analytics/>
     </Provider>
   </>
 }
